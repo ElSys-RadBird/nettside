9a64f7c63607135f6b232a73fbbe86d505080f27
Niklad <60095505+Niklad@users.noreply.github.com>
2020-02-18 00:29:22 +0100

    La kartet sin script i egen fil

---
+++ js/Kart.js
@@ -0,0 +1,59 @@
+// An object containing the position and radius of our nodes
+var nodeMap = {
+    blitzerman: {
+      center: {lat: 63.421550, lng: 10.12148},
+      diameter: 100,
+      text: '1'
+    },
+    gazerbeam: {
+      center: {lat: 63.42000, lng: 10.11946},
+      diameter: 100,
+      text: '2'
+    },
+    stormicide: {
+      center: {lat: 63.42000, lng: 10.12350},
+      diameter: 100,
+      text: '3'
+    }
+  };
+
+
+
+
+
+  function initMap() {
+    // Create the map
+    var map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 63.42092, lng: 10.12167},
+      zoom: 14.5,
+      mapTypeId: 'satellite'
+    });
+
+    // Construct the circles
+    for (var node in nodeMap) {
+      var nodeCircle = new google.maps.Circle({
+        strokeColor: '#FF0000',
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: '#FF0000',
+        fillOpacity: 0.35,
+        map: map,
+        center: nodeMap[node].center,
+        radius: nodeMap[node].diameter
+      });
+      //var mapLabel = new google.maps.mapLabel({
+      //text: nodeMap[node].text,
+      //position: nodeMap[node].center,
+      //map: map,
+      //fontSize: 35,
+      //align: 'center'
+      //});
+      //mapLabel.set('position', new google.maps.LatLng(nodeMap[node].center));
+
+      //var marker = new google.maps.Marker();
+      //marker.bindTo('map', mapLabel);
+      //marker.bindTo('position', mapLabel);
+    }
+  }
+  // The following script contains an unrestricted API key connected to my PERSONAL google cloud console profile. 
+  // It will charge me economically if overused. Therefore: Do NOT spread it around - Andreas
