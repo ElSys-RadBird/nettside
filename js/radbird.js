37c29f24805d3ec2b512e2ef1bb4ca09bc01009f
Niklad <60095505+Niklad@users.noreply.github.com>
2020-02-18 01:59:25 +0100

    Div. forskning på graf

--- js/radbird.js
+++ js/radbird.js
@@ -11,73 +11,77 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
+// Lager en databasevariabel
 var db =firebase.database();
 
-var ctx = document.getElementById('myChart');
-function myChart(data_set){ new Chart(ctx, {
-type: 'bar',
-data: {
-labels: ['Node 1 ', 'Node 2', 'Node 3'],
-datasets: [{
-label: 'Antall minutter med fugleaktivitet',
-data: data_set,
-backgroundColor: [
-  'rgba(255, 99, 132, 0.2)',
-  'rgba(54, 162, 235, 0.2)',
-  'rgba(255, 206, 86, 0.2)'
-  
-],
-borderColor: [
-  'rgba(255, 99, 132, 1)',
-  'rgba(54, 162, 235, 1)',
-  'rgba(255, 206, 86, 1)'
-  
-],
-borderWidth: 1
-}]
-},
-options: {
-scales: {
-yAxes: [{
-  ticks: {
-      beginAtZero: true
-  }
-}]
-}
-}
-});
-}
-
+// Henter nodedata
 function get_node_data(node,tid){
-var lengde_node= 0;
-var ref =firebase.database().ref(node)
-ref.orderByChild('tid').endAt(tid).on('value',function(snapshot){
-var node_data=snapshot.val();
-console.log(node_data)
-for(var key in node_data ){
-if (key=="aktivitet"){
-lengde_node=0;
-break
-}
-else{
-if(node_data.hasOwnProperty(key)){
-lengde_node++
+  var lengde_node= 0;
+  var ref =firebase.database().ref(node)
+  ref.orderByChild('tid').endAt(tid).on('value',function(snapshot){
+    var node_data=snapshot.val();
+    console.log(node_data)
+    for(var key in node_data ){
+      if (key=="aktivitet"){
+        lengde_node=0;
+        break
+      }
+      else{
+        if(node_data.hasOwnProperty(key)){
+          lengde_node++ 
+        }
+      }
+    }
+  })
+  console.log(lengde_node);
+  return lengde_node;
 }
 
-}
-}
-})
-console.log(lengde_node)
-return lengde_node;
-}
-
+// Henter data og lager et datasett (?)
 function get_data(tid){
-var data_set=[]
-data_set.push(get_node_data("node1/",tid))
-data_set.push(get_node_data("node2/",tid))
-data_set.push(get_node_data("node3/",tid))
-console.log(data_set)
-return data_set;
+  var data_set=[]
+  data_set.push(get_node_data("node1/",tid))
+  data_set.push(get_node_data("node2/",tid))
+  data_set.push(get_node_data("node3/",tid))
+  console.log(data_set)
+  return data_set;
 }
-myChart(get_data(4)); 
-myChart(get_data(4));     
+
+// Lager grafen
+
+function myChart(data_set){ 
+  var ctx = document.getElementById('myChart');
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Node 1 ', 'Node 2', 'Node 3'],
+      datasets: [{
+        label: 'Antall ganger observert fugler',
+        data: data_set,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+    }
+  });
+}
+
+
+myChart(0);
